1339ed8b3c6338e51d9b67554545d5fd5b7395b7
Ivan Stepaniuk <istepaniuk@gmail.com>
2018-08-10 11:08:57 +0200

    initial commit : empty test runner

---
+++ tests/RoverTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class RoverTest extends TestCase
+{
+    public function test_nothing()
+    {
+        self::assertEquals(true, false);
+    }
+}
